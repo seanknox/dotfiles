96e2f18ec55385afdbf76a32716e4c95331ba7e0
Sean Knox <knoxville@gmail.com>
2020-05-05 10:45:42 -0700

    add zsh syntax highlighting

---
+++ install-zsh-syntax-highlighting.sh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+# Navigate the a folder where we can download the zsh-syntax-highlighting script
+
+# Note: if the zsh folder does not exists in /usr/local/share, just create it
+
+(
+  cd /usr/local/share/zsh || { echo "/usr/local/share/zsh does not exist"; exit 1; }
+
+  # Clone the zsh-syntax-highlighting repo
+
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
+
+  # Add a startup script to your .zshrc to enable syntax highlighting in new shell sessions
+
+  echo "source ${(q-)PWD}/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "${HOME}/.zshrc"
+)
