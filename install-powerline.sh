e453dad292c66c48370d03cdb3b1d74751f870a6
Sean Knox <knoxville@gmail.com>
2020-05-05 22:19:54 -0700

    remove manual powerline font install

--- install-powerline.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mkdir -p ~/src
-(
-  cd ~/src || { echo "couldn't switch to src/. does ~/src/src exist?"; exit 1; }
-  git clone https://github.com/powerline/fonts.git --depth=1
-  cd fonts || { echo "couldn't switch to fonts/. does ~/src/fonts exist?"; exit 1; }
-  ./install.sh
-)
