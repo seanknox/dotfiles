c05bd72788ff01c0822027cfb1890cdafe133c42
Sean Knox <knoxville@gmail.com>
2020-08-26 14:18:44 -0700

    ensure newlines when writing to zshrc

--- install-z.sh
+++ install-z.sh
@@ -3,5 +3,5 @@
 mkdir -p /usr/local/etc/profile.d || { echo "couldn't create /usr/local/etc/profile.d"; exit 1; }
 curl -sk https://raw.githubusercontent.com/rupa/z/master/z.sh >> /usr/local/etc/profile.d/z.sh
 
-echo "# shellcheck disable=SC1091
-source /usr/local/etc/profile.d/z.sh" >> "${HOME}/.zshrc"
+echo "\n# shellcheck disable=SC1091" >> "${HOME}/.zshrc"
+echo "source /usr/local/etc/profile.d/z.sh" >> "${HOME}/.zshrc"
