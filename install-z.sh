53fe6dd612944990a24d413d1f4f2586826c6b5d
Sean Knox <knoxville@gmail.com>
2023-08-08 13:52:40 -0700

    update z.sh paths on newer macos

--- install-z.sh
+++ install-z.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-mkdir -p /usr/local/etc/profile.d || { echo "couldn't create /usr/local/etc/profile.d"; exit 1; }
-curl -sk https://raw.githubusercontent.com/rupa/z/master/z.sh >> /usr/local/etc/profile.d/z.sh
+mkdir -p /opt/homebrew/etc/profile.d || { echo "couldn't create /opt/homebrew/etc/profile.d"; exit 1; }
+curl -sk https://raw.githubusercontent.com/rupa/z/master/z.sh >> /opt/homebrew/etc/profile.d/z.sh
 
 echo "\n# shellcheck disable=SC1091" >> "${HOME}/.zshrc"
-echo "source /usr/local/etc/profile.d/z.sh" >> "${HOME}/.zshrc"
+echo "source /opt/homebrew/etc/profile.d/z.sh" >> "${HOME}/.zshrc"
